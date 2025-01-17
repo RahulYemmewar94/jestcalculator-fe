136a22614616aff174fad35d88dd9d26fc861d05
Yemmewarrahul <rahul.yemmewar>
2025-01-17 14:14:12 +0530

    failed test for addition

---
+++ src/calculator/calculator.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const CalculatorWithJestTestCases = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleCalculations = (value) => {
+   console.log('value', value);
+   
+  };
+  
+
+  return (
+    <>
+      <div className="calc">
+        <div className="display" data-testid="result">{inputValue || "0"}</div>
+        <div className="buttons">
+          <button data-testid="7" onClick={() => handleCalculations("7")}>7</button>
+          <button data-testid="8" onClick={() => handleCalculations("8")}>8</button>
+          <button data-testid="9" onClick={() => handleCalculations("9")}>9</button>
+          <button data-testid="4" onClick={() => handleCalculations("4")}>4</button>
+          <button data-testid="5" onClick={() => handleCalculations("5")}>5</button>
+          <button data-testid="6" onClick={() => handleCalculations("6")}>6</button>
+          <button data-testid="1" onClick={() => handleCalculations("1")}>1</button>
+          <button data-testid="2" onClick={() => handleCalculations("2")}>2</button>
+          <button data-testid="3" onClick={() => handleCalculations("3")}>3</button>
+          <button data-testid="0" onClick={() => handleCalculations("0")}>0</button>
+          <button data-testid="." onClick={() => handleCalculations(".")}>.</button>
+          <button data-testid="+" onClick={() => handleCalculations("+")}>+</button>
+          <button data-testid="=" onClick={() => handleCalculations("=")}>=</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CalculatorWithJestTestCases;
--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -2,9 +2,18 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import CalculatorWithJestTestCases from "./calculator";
 
 describe("Calculator Test Cases", () => {
-  test("test", async () => {
-    render(<CalculatorWithJestTestCases />);
-   
+    test("Addition", async () => {
+      render(<CalculatorWithJestTestCases />);
+      
+      fireEvent.click(screen.getByText("2"));
+      fireEvent.click(screen.getByText("1"));
+      fireEvent.click(screen.getByText("+"));
+      fireEvent.click(screen.getByText("6"));
+      fireEvent.click(screen.getByText("1"));
+      fireEvent.click(screen.getByText("5"));
+      fireEvent.click(screen.getByText("="));
+      
+      const result = await screen.findByTestId("result"); 
+      expect(result).toHaveTextContent("636"); 
+    });
   });
-
-});
