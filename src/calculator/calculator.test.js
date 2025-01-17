e88fb2ba5a2c4d48088101de5e04803a9c3c99b0
Yemmewarrahul <rahul.yemmewar>
2025-01-17 13:54:20 +0530

    component and test file is created test is failed for component rendering

---
+++ src/calculator/calculator.test.js
@@ -0,0 +1,10 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import CalculatorWithJestTestCases from "./calculator";
+
+describe("Calculator Test Cases", () => {
+  test("test", async () => {
+    render(<CalculatorWithJestTestCases />);
+   
+  });
+
+});
